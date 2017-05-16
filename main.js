89f8076f4903d05b352ec96974e2e5c2d95746e5
apeachey <apeachey@audigygroup.com>
2017-05-16 13:20:12 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,34 +1,38 @@
 var scorePlayer = 0;
 var scoreComp = 0;
-var ranNumbP = Math.floor(Math.random() * 13);
-var ranNumbC = Math.floor(Math.random() * 13);
+var ranNumbP = Math.floor(Math.random() * 1000);
+var ranNumbC = Math.floor(Math.random() * 1000);
 
 function startGame () {
-alert('Start game?');
-while (scorePlayer < 10 || scoreComp < 10) {
+alert('The battle has begun.');
+while (scorePlayer < 10 && scoreComp < 10) {
 if (ranNumbP > ranNumbC) {
-    console.log('Player drew a ' + ranNumbP + ' Computer drew a ' + ranNumbC);
-    console.log('Point for you!');
+    console.log('Your forces killed ' + ranNumbP + ' Goblins. The Goblins killed ' + ranNumbC + ' Elves.');
+    console.log('You have won this battle!');
     scorePlayer++;
-    console.log('Score Player: ' + scorePlayer + ' Score Computer: ' + scoreComp);
-    ranNumbP = Math.floor(Math.random() * 13);
-    ranNumbC = Math.floor(Math.random() * 13);
+    console.log('Battles won by Elves: ' + scorePlayer + ' --- Battles won by Goblins: ' + scoreComp);
+    ranNumbP = Math.floor(Math.random() * 1000);
+    ranNumbC = Math.floor(Math.random() * 1000);
 } else if (ranNumbC > ranNumbP) {
-    console.log('Player drew a ' + ranNumbP + ' Computer drew a ' + ranNumbC);
-    console.log('Point for the computer!');
+    console.log('Your forces killed ' + ranNumbP + ' Goblins. The Goblins killed ' + ranNumbC + ' Elves.');
+    console.log('The Goblins won this battle!');
     scoreComp++;
-    console.log('Score Player: ' + scorePlayer + ' Score Computer: ' + scoreComp);
-    ranNumbP = Math.floor(Math.random() * 13);
-    ranNumbC = Math.floor(Math.random() * 13);
+    console.log('Battles won by Elves: ' + scorePlayer + ' --- Battles won by Goblins: ' + scoreComp);
+    ranNumbP = Math.floor(Math.random() * 1000);
+    ranNumbC = Math.floor(Math.random() * 1000);
 } else {
-    console.log('Player drew a ' + ranNumbP + ' Computer drew a ' + ranNumbC);
-    console.log('Draw!');
-    console.log('Score Player: ' + scorePlayer + ' Score Computer: ' + scoreComp);
-    ranNumbP = Math.floor(Math.random() * 13);
-    ranNumbC = Math.floor(Math.random() * 13);
+    console.log('Your forces killed ' + ranNumbP + ' Goblins. The Goblins killed ' + ranNumbC + ' Elves.');
+    console.log('You and the Goblin\'s lost equal forces. Both retreat for the next battle.');
+    console.log('Battles won by Elves: ' + scorePlayer + ' --- Battles won by Goblins: ' + scoreComp);
+    ranNumbP = Math.floor(Math.random() * 1000);
+    ranNumbC = Math.floor(Math.random() * 1000);
 }
     
-    alert('End of round! Continue?');
+    if (ranNumbP > ranNumbC) {
+        alert('Your forces were victorious in this battle. The Goblins are regrouping. Ready for the next battle?');
+    } else {
+        alert('The Goblins defeated your forces in battle. Time to regroup and plan another attack. Ready for the next battle?');
+    }
     continue;
   
     scorePlayer === 10 || scoreComp === 10; 
@@ -37,9 +41,9 @@ if (ranNumbP > ranNumbC) {
     
     //Declare Winner
     if (scorePlayer > scoreComp) {
-        console.log ('You win!');
+        alert('The war is over, you have won! You present the sword of the Goblin King to the Elvish Council. Be proud hero.');
     } else {
-        console.log ('Computer wins!');
+        alert('The Goblins have won the war. You have brought great shame to the elves!');
     }
+    console.log('GAME OVER')
 }
-
